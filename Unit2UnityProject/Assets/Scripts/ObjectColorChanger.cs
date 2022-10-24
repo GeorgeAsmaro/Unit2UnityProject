0effe3190b3fdaa1454a6b1bac4bde4d79e7f0e5
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-24 12:48:33 -0700

    update

--- Unit2UnityProject/Assets/Scripts/ObjectColorChanger.cs
+++ Unit2UnityProject/Assets/Scripts/ObjectColorChanger.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class ObjectColorChanger : MonoBehaviour
 {
+    bool blue = false;
+    bool continues = true;
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.gameObject.tag == "ColorChange")
+
+        if (hit.gameObject.tag == "ColorChange" && !blue)
         {
-            hit.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            hit.gameObject.GetComponent<Renderer>().material.color = new Color(0f, 0f, 1f, 0f);
+            blue = true;
+        }
+
+        if(hit.gameObject.tag == "ColorChange" && blue)
+        {
+            hit.gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 1f);
+            blue = false;
         }
     }
 
-    
 }
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ObjectColorChanger : MonoBehaviour
+//{
+//    //private Color renderer;
+
+
+//    private void OnControllerColliderHit(ControllerColliderHit hit)
+//    {
+//        //renderer = GetComponent<Renderer>;
+//        if (hit.gameObject.tag == "ColorChange")
+//        {
+//            Color blueColour = new Color(0f, 0f, 1f, 0f);
+//            hit.gameObject.GetComponent<Renderer>().material.color = blueColour;
+//            //GameObject.Find("Square1").GetComponent<Renderer>().material.color = new Color(0f, 0f, 1f, 0f);
+//            Debug.Log("Hit");
+//        }
+//    }
+
+
+//}
+

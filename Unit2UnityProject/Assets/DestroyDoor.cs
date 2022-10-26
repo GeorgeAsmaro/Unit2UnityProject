87777138a4e03f55154b13e7d6e143b071286b9a
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-26 13:26:17 -0700

    updates to world and attempts at destroy script, but nothing yet

--- Unit2UnityProject/Assets/DestroyDoor.cs
+++ Unit2UnityProject/Assets/DestroyDoor.cs
@@ -9,34 +9,62 @@ public class DestroyDoor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject stl = GameObject.Find("ColorChange");
-        ObjectColorChanger cc = stl.GetComponent<ObjectColorChanger>();
+        GameObject stl = new GameObject();
+        bool blue = stl.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject stm = GameObject.Find("ColorChange2");
-        ObjectColorChanger cc2 = stm.GetComponent<ObjectColorChanger>();
+        GameObject stm = new GameObject();
+        bool blue2 = stm.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject str = GameObject.Find("ColorChange3");
-        ObjectColorChanger cc3 = str.GetComponent<ObjectColorChanger>();
+        GameObject str = new GameObject();
+        bool blue3 = str.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject sml = GameObject.Find("ColorChange4");
-        ObjectColorChanger cc4 = sml.GetComponent<ObjectColorChanger>();
+        GameObject sml = new GameObject();
+        bool blue4 = sml.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject smm = GameObject.Find("ColorChange5");
-        ObjectColorChanger cc5 = smm.GetComponent<ObjectColorChanger>();
+        GameObject smm = new GameObject();
+        bool blue5 = smm.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject smr = GameObject.Find("ColorChange6");
-        ObjectColorChanger cc6 = smr.GetComponent<ObjectColorChanger>();
+        GameObject smr = new GameObject();
+        bool blue6 = smr.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject sbl = GameObject.Find("ColorChange7");
-        ObjectColorChanger cc7 = sbl.GetComponent<ObjectColorChanger>();
+        GameObject sbl = new GameObject();
+        bool blue7 = sbl.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject sbm = GameObject.Find("ColorChange8");
-        ObjectColorChanger cc8 = sbm.GetComponent<ObjectColorChanger>();
+        GameObject sbm = new GameObject();
+        bool blue8 = sbm.GetComponent<ObjectColorChanger>().getBlue();
 
-        GameObject sbr = GameObject.Find("ColorChange9");
-        ObjectColorChanger cc9 = sbr.GetComponent<ObjectColorChanger>();
+        GameObject sbr = new GameObject();
+        bool blue9 = sbr.GetComponent<ObjectColorChanger>().getBlue();
 
-        if (cc.getBlue() && !cc2.getBlue() && cc3.getBlue() && !cc4.getBlue() && cc5.getBlue() && !cc6.getBlue() && cc7.getBlue() && !cc8.getBlue() && cc9.getBlue())
+
+        //GameObject stl = GameObject.Find("ColorChange");
+        //ObjectColorChanger cc = stl.GetComponent<ObjectColorChanger>();
+
+        //GameObject stm = GameObject.Find("ColorChange2");
+        //ObjectColorChanger cc2 = stm.GetComponent<ObjectColorChanger>();
+
+        //GameObject str = GameObject.Find("ColorChange3");
+        //ObjectColorChanger cc3 = str.GetComponent<ObjectColorChanger>();
+
+        //GameObject sml = GameObject.Find("ColorChange4");
+        //ObjectColorChanger cc4 = sml.GetComponent<ObjectColorChanger>();
+
+        //GameObject smm = GameObject.Find("ColorChange5");
+        //ObjectColorChanger cc5 = smm.GetComponent<ObjectColorChanger>();
+
+        //GameObject smr = GameObject.Find("ColorChange6");
+        //ObjectColorChanger cc6 = smr.GetComponent<ObjectColorChanger>();
+
+        //GameObject sbl = GameObject.Find("ColorChange7");
+        //ObjectColorChanger cc7 = sbl.GetComponent<ObjectColorChanger>();
+
+        //GameObject sbm = GameObject.Find("ColorChange8");
+        //ObjectColorChanger cc8 = sbm.GetComponent<ObjectColorChanger>();
+
+        //GameObject sbr = GameObject.Find("ColorChange9");
+        //ObjectColorChanger cc9 = sbr.GetComponent<ObjectColorChanger>();
+
+        if (blue && blue2 && blue3 && blue4 && blue5 && blue6 && blue7 && blue8 && blue9)
         {
             Debug.Log("Destroy this");
             //Destroy(this);
@@ -49,4 +77,5 @@ public class DestroyDoor : MonoBehaviour
     {
         
     }
+
 }

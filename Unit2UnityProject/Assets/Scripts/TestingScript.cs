3c9e93de9afb5843f40af1b9c39d51b80451218c
George Asmaro <george.asmaro@hotmail.com>
2022-11-16 23:38:23 -0800

    tests

---
+++ Unit2UnityProject/Assets/Scripts/TestingScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    string forsen = "forsen";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(forsen); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
